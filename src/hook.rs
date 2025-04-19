189e4428b30e27a5d7d5ced3fcec1ca147b09389
Target-san <target.san@gmail.com>
2025-04-19 21:46:20 +0300

    Reshuffle tests, use special test macro to create subprocess tests

--- src/hook.rs
+++ src/hook.rs
@@ -114,6 +114,8 @@ fn defer<F: FnOnce()>(f: F) -> impl Drop {
 
 #[cfg(test)]
 mod tests {
+    use crate::subprocess_test;
+
     use super::*;
 
     #[test]
@@ -146,5 +148,40 @@ mod tests {
         .unwrap_err();
 
         assert_eq!(counter, 1);
+    }
+
+    subprocess_test! {
+        #[test]
+        fn hook_forwarding() {
+            use crate::hook::{NextHook, catch_unwind_with_scoped_hook};
+            use std::panic::{PanicHookInfo, set_hook};
+            use std::sync::atomic::{AtomicUsize, Ordering};
+    
+            static GLOB_COUNTER: AtomicUsize = AtomicUsize::new(0);
+    
+            fn counter_hook(_info: &PanicHookInfo<'_>) {
+                GLOB_COUNTER.fetch_add(1, Ordering::Relaxed);
+            }
+            // If we return [`NextHook::PrevInstalledHook`] from hook,
+            // previous installed hook should be actually invoked
+            //
+            // Must be in separate binary because it overrides default hook
+            // prior to scoped one takes effect
+            set_hook(Box::new(counter_hook));
+    
+            let mut counter = 0;
+    
+            let result = catch_unwind_with_scoped_hook(
+                |_| {
+                    counter += 1;
+                    NextHook::PrevInstalledHook
+                },
+                || panic!("Oops!"),
+            );
+            assert!(result.is_err());
+    
+            assert_eq!(counter, 1);
+            assert_eq!(GLOB_COUNTER.load(Ordering::Relaxed), 1);
+        }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -36,5 +36,7 @@ pub mod hook;
 /// Panic capture types and functions,
 /// including some with finer tuned functionality
 pub mod panic;
+#[cfg(test)]
+mod subprocess_test;
 
 pub use panic::{Panic, catch_panic};
